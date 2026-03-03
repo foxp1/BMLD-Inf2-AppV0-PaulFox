1d9e4bfa5c3485b3f573bf86bc0fa57d79a4d36f
Paul Fox <foxp@zhaw.ch>
2026-03-03 21:39:01 +0100

    removed default text from num 1

--- views/rechner.py
+++ views/rechner.py
@@ -7,7 +7,7 @@ with st.form("add_form"):
     st.write("Enter the numbers to add")
 
 
-    number1 = st.text_input("Number 1", "Enter your number here...")
+    number1 = st.text_input("Number 1")
 
 
     number2 = st.slider("Form slider")
