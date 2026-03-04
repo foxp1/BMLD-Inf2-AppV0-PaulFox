c781a299f970bdf221120bf7f6970923ea3639ed
Paul Fox <foxp@zhaw.ch>
2026-03-04 09:45:07 +0100

    start again

--- views/temp_rechner.py
+++ views/temp_rechner.py
@@ -1,14 +1,3 @@
 import streamlit as st
-from functions.temperatur import celsius_to_fahrenheit
 
-st.title("Temperatur Rechner")
-
-st.write("Mein Rechner für die Umrechnung von Celsius zu Fahrenheit")
-
-with st.form("my_form"):
-    celsius = st.number_input("Gib die Temperatur in Celsius ein:")
-    submit_button = st.form_submit_button("Umrechnen")
-    if submit_button:
-        fahrenheit = celsius_to_fahrenheit(celsius)
-        st.write(f"Die Temperatur in Fahrenheit ist: {fahrenheit}")
-st.write("Outside the form")
+st.title("Rechner")
